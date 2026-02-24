d1d661030da9c9a80baacd5cc902cc9d8bdfeb6c
saketh42 <116485868+saketh42@users.noreply.github.com>
2026-02-24 16:37:20 +0530

    feat: qol change

--- bolna/bolna_fetch.py
+++ bolna/bolna_fetch.py
@@ -12,4 +12,10 @@ class BolnaFetch:
         else:
             response.raise_for_status()
 
-            
+if __name__ == "__main__":
+    execution_id = "af1e7820-3342-41fe-bd7b-e44049ca1c9f"
+    auth_token = "bn-3cab50fdf99b4f85909e7fcd465f76b5"
+    
+    bolna_fetcher = BolnaFetch(execution_id, auth_token)
+    output = bolna_fetcher.fetch_output()
+    print(output)
